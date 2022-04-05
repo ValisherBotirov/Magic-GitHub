5762fca6227ea00e9e3fb293e89d9df72f12f484
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-04-05 18:18:34 +0500

    The finished project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const searchInput = document.querySelector(".user__input");
 const error = document.querySelector(".errorPage");
 const head = document.querySelector(".head");
+const main = document.querySelector(".main");
 
 const gitHubUsers = async function (user) {
   // try {
@@ -12,7 +13,6 @@ const gitHubUsers = async function (user) {
   // if (!data.ok) {
   //   throw new Error();
   // }
-  console.log(data);
   let html = `
     <div class="box user__info">
     <div class="user__img">
@@ -29,22 +29,52 @@ const gitHubUsers = async function (user) {
       </div>
       <div class="infoo">
         <div class="table__info">
-          <p class="about__text">Company: ${data.company}</p>
+          <p class="about__text">Company: ${
+            data.company == null ? "Yo`q" : data.company
+          }</p>
           <p class="about__text">Website/Blog: ${data.blog}</p>
           <p class="about__text">Location: ${data.location}</p>
-          <p class="about__text yes__border">Member Since: ${data.created_at}</p>
+          <p class="about__text yes__border">Member Since: ${
+            data.created_at
+          }</p>
         </div>
       </div>
     </div>
   </div>
     `;
-  head.innerHTML = html;
+  // head.innerHTML = "";
+  // head.insertAdjacentHTML("beforeend", html);
+  head.innerHTML = html; //yoki
+
   // } catch (err) {
   //   error.style.display = "flex";
   //   // document.querySelector("body").style.overflow = "hiddin";
   // }
 };
 
+const getUserPro = async function (user) {
+  const dataJson = await fetch(
+    `https://api.github.com/users/${user}/repos?per_page=created: asc&sort=5&client_id=65b44d46d520be1f19c7&client_secret=7287ef205413001a79b30f0fbcc04416153ef797`
+  );
+  const data = await dataJson.json();
+  console.log(data);
+  // main.innerHTML = "";
+  data.forEach(function (val) {
+    let html = `
+    <div class="box repo">
+   <a href="${val.clone_url}" class="repo__name">${val.name}</a>
+   <div class="report">
+     <p class="stat stat__1">Starts:${val.stargazers_count}</p>
+     <p class="stat stat__2">Watchers: ${val.watchers}</p>
+     <p class="stat stat__3">Forks: ${val.forks}</p>
+   </div>
+ </div>
+   `;
+    main.insertAdjacentHTML("beforeend", html);
+  });
+};
+
 searchInput.addEventListener("input", function () {
   gitHubUsers(searchInput.value);
+  getUserPro(searchInput.value);
 });
